235edab317c9317b2cfe334a528016927869e95c
Eman_devs <emansalahuddinnajmi@gamil.com>
2023-04-13 02:57:30 +0500

    feat:java assignment

---
+++ app.js
@@ -0,0 +1,70 @@
+// Task 1
+
+var zakatPercentage = 0.025;
+var userInput = Number( prompt("Enter a number"));
+var result = (zakatPercentage) * (userInput);
+
+// task 5
+
+var contactsNO = []
+var contactsName = []
+
+
+for (var a =0 ; a<3 ; a++)
+{
+    var cNO = prompt('Number')
+    var Cname = prompt('Name')
+}
+
+
+contactsNO.push('cNo')
+contactsName.push('cName')
+
+
+console .log(contactsNO)
+console.log(contactsName)
+
+// Task 4
+
+ var userName = prompt('Enter your Name Please')
+console.log( userName [0] )
+userName[0] = (user[0].uppercase)
+alert( userName)
+
+// Task 7
+
+var age = 18;
+var Nationality = prompt("Enter your Nationality")
+var age = +prompt("Enter your age")
+
+if(Nationality=='pakistani'|| Nationality=='Pakistani' ){
+    
+    if(age>=18){
+        alert("pease go to your voting area")
+    }
+    else{
+        console.log("your are uder age")
+    }
+
+}
+
+
+
+else if(Nationality=='indian'|| Nationality=='Indian'){
+
+    alert("your are not eligible to vote in pakistan")
+}
+
+else if(Nationality=='American'|| Nationality=='american'){
+
+    alert("your are not eligible to vote in pakistan")
+}
+
+
+
+
+
+
+
+
+
