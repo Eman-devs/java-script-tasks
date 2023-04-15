c71974e6657211113ef7c56032883455bb183ef3
Eman_devs <emansalahuddinnajmi@gamil.com>
2023-04-16 00:22:48 +0500

    java tasks

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
-// Task 1
+// DAY 1, 12 APRIL  TASK NO 1
 
-var zakatPercentage = 0.025;
-var userInput = Number( prompt("Enter a number"));
-var result = (zakatPercentage) * (userInput);
+// var zakatPercentage = 0.025;
+// var userInput = Number( prompt("Enter a number"));
+// var result = (zakatPercentage) * (userInput);
 
-// task 5
+// // task 5
 
-var contactsNO = []
-var contactsName = []
-
-
-for (var a =0 ; a<3 ; a++)
-{
-    var cNO = prompt('Number')
-    var Cname = prompt('Name')
-}
+// var contactsNO = []
+// var contactsName = []
 
 
-contactsNO.push('cNo')
-contactsName.push('cName')
+// for (var a =0 ; a<3 ; a++);
+// {
+//     var cNO = prompt('Number');
+//     var Cname = prompt('Name');
+// }
 
 
-console .log(contactsNO)
-console.log(contactsName)
+// contactsNO.push('cNo');
+// contactsName.push('cName');
 
-// Task 4
 
- var userName = prompt('Enter your Name Please')
-console.log( userName [0] )
-userName[0] = (user[0].uppercase)
-alert( userName)
+// console .log(contactsNO);
+// console.log(contactsName);
 
-// Task 7
+// // Task 4
+
+// var user = prompt("enter your Name");
+// console.log(user)
+// var letter = user.charAt(0);
+// console.log (letter)
+// console.log (latter.toUppercase());
+// var run = user.slice(1);
+// console.log(run);
+// consol.log(run.toLowerCase());
+// var cap = letter.toUpperCase( ) + run.toLowerCase( );
+// console.log( caps);
+// alert(caps)
+
+
+
+// // Task 7
 
 var age = 18;
 var Nationality = prompt("Enter your Nationality")
@@ -61,9 +70,74 @@ else if(Nationality=='American'|| Nationality=='american'){
 }
 
 
+// TASK NO 2
+
+// var FamilyMembers = +prompt("Enter your total family members")
+
+// var FitraMethod = +prompt("choose Fitra Method:\1 Wheat-250 Rupees \2 dates-2100 Rupees \3 Jau-450 Rupees \4 Kishmish-2800 Rupees" )
+
+// var Fitraprices;
+
+// if(FitraMethod == "1"){
+//     Fitraprices = 250;
+// } else if(FitraMethod == "2"){
+//     Fitraprices = 2100;
+// } else if(FitraMethod == "3"){
+//     Fitraprices = 450;
+// } else if(FitraMethod == "4"){
+//     Fitraprices = 2800;
+// }
 
 
+// else {
+//     alert("PLEASE SELECT THE VALID METHOD")
+// }
 
+// var fitraAmount = FamilyMembers * Fitraprices
+
+// alert("The fitra amount for"+ FamilyMembers + "Family member using the" + FitraMethod + "method is RS" + fitraAmount );
+
+// TASK NO 3
+
+// var SecertNumber = +prompt("Guess the Numder between 1 to 10");
+// if(SecertNumber == 5){
+//     alert("Congratulation your guess the right number")
+// }
+
+// else if(SecertNumber <=5){
+//     alert("your guess is too closed")
+// }
+// else {
+//     alert(" guess again")
+// }
+
+// TASK NO 6
+
+// var products = ["dark chocolat shake", "oreo shake", "browny  shake", "", " vanilla ice cream", "milk soda"];
+
+
+// var userInput = parseInt(prompt(`Enter the position of the item you want to remove (1 - ${products.length}):`));
+
+// var removedItem = products.splice(userInput - 1, 1);
+
+// console.log(`The removed item is: ${removedItem}`);
+// alert(`The removed item is: ${removedItem}`);
+
+// console.log(`The remaining items in the array are: ${products}`);
+// alert(`The remaining items in the array are: ${products}`);
+
+// console.log(`Total number of items remaining: ${products.length}`);
+// alert(`Total number of items remaining: ${products.length}`);
+
+// TASK NO 8
+
+// var WorldCupSquad = ["player01","player02","player03","player04","player05","player06","player07","player08","player09","player10","player11","player12","player13","player14","player15",]
+// var WorldCupteam  = ["player01","player02","player03","player04","player05","player06","player07","player08","player09","player10","player11","player12","player18",]
+
+// var FinalTeam = WorldCupSquad.slice(3,13)
+
+// alert("WorldCupSquad")
+// alert("FinalTeam")
 
 
 
